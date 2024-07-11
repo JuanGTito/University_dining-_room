6475372dd96b88ed837f167b05dbb0c9af8a4d31
unknown <juangtito.hck@gmail.com>
2024-07-11 15:03:31 -0500

    update and create file js

--- conection/conexion.js
+++ conection/conexion.js
@@ -1,9 +1,10 @@
-const Connection = require('mysql');  
-var config = Connection.createConnection({
+function conexion(){
+    const Connection = require('mysql');  
+    var config = Connection.createConnection({
     host: "localhost",
     database: "comedor_universitario",
-    user: "admin",
-    password: "admin"
+    user: "admin", //admin
+    password: "admin" //admin
 });
 
 config.connect(function(err){
@@ -12,4 +13,9 @@ config.connect(function(err){
     } else{
         console.log("conexion exitosa")
     }
+    
 });
+config.query('SELECT * FROM estudiante WHERE dni = "60177245"', (error, resultados) => 
+    { if (error) throw error; console.log('Registros de la tabla "usuarios":', resultados); });
+  };
+  conexion();
