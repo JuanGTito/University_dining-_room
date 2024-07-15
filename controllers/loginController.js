ca795c2fe45ec8c9b3cc8a986d7282e84bee8551
JuanGtito <juangtito.hck@gmail.com>
2024-07-15 00:23:37 -0500

    update file controllers

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,13 +1,62 @@
+const bcrypt = require('bcrypt');
+
 function login(req, res) {
-    res.render('login/index')
-}
-
-function storeUser(req, res) {
+    if (req.session.loggedin) {
+          if(req.session.loggedin != true)
+      res.redirect('login/index');
+      
+    } else {
+      res.render('/');
+    }
+  }
+  
+  function register(req, res) {
+    res.render('login/register');
+  }
+  
+function auth(req, res) {
     const data = req.body;
-    console.log(data)
-}
+  
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM users WHERE email = ?', [data.email], (err, userdata) => {
+            if(userdata.length > 0) {
+                userdata.forEach(element => {
+                    bcrypt.compare(data.password, element.password, (err, isMatch) => {
+                        if (!isMatch) {
+                            res.render('login/index', {error: 'error: incorrect password'});
+                        } else {
+                            req.session.loggedin = true;
+                            req.session.name  = element.name;
 
-module.exports = {
-    login,
-    storeUser
-}
+                            res.redirect('/')
+
+                        }
+                    });
+                });
+                } else {
+                    res.render('login/index', {error: 'error: user not exists !'})
+                    }
+        /*
+        req.session.loggedin = true;
+      req.session.name = name;
+  
+    res.redirect('/');*/
+        
+      });
+    });
+  }
+  
+  function logout(req, res) {
+    if (req.session.loggedin == true) {
+      req.session.destroy();
+    }
+    res.redirect('/');
+  }
+  
+  
+  module.exports = {
+    login: login,
+    register: register,
+    auth: auth,
+    logout: logout,
+  }
