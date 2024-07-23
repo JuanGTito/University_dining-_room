901784840d4c2964493082251d4d11ca22bb6d01
JuanGtito <juangtito.hck@gmail.com>
2024-07-23 11:10:29 -0500

    update router search for register why faild in views

--- router.js
+++ router.js
@@ -11,7 +11,7 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-//router.get('/search', supervisarController);
+router.get('/register', supervisarController);
 
 router.get('/register', (req, res) => {
 	
